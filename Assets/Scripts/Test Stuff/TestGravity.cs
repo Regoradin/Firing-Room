d905855d64b573821648e3b3deb5080fb6209ed1
Regoradin <oliver@theabates.com>
2017-08-22 23:53:42 -0500

    Rocket Orbit Test
    Small test of orbital mechanics to see how badly this can get messed up.
    (Very badly is the answer)

---
+++ Assets/Scripts/Test Stuff/TestGravity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGravity : MonoBehaviour {
+
+	public GameObject gravitied;
+	public float grav_constant;
+
+	private void FixedUpdate()
+	{
+		Vector3 direction = (transform.position - gravitied.transform.position).normalized;
+		float radius = Vector3.Distance(transform.position, gravitied.transform.position);
+		
+
+		Vector3 directed_force = direction * (grav_constant * (gameObject.GetComponent<Rigidbody>().mass) * (gravitied.GetComponent<Rigidbody>().mass)) / (radius * radius);
+
+		gravitied.GetComponent<Rigidbody>().AddForce(directed_force);
+	}
+
+}
---
+++ Assets/Scripts/Test Stuff/TestRocketController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRocketController : MonoBehaviour {
+
+	public float rotation;
+	public float force;
+
+	private void FixedUpdate()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			Thrust(force);
+		}
+
+
+		Turn(rotation * Input.GetAxis("Horizontal"));
+	}
+
+	public void Turn(float rotation)
+	{
+		transform.Rotate(Vector3.back * rotation);
+	}
+
+	public void Thrust(float force)
+	{
+		transform.GetComponent<Rigidbody>().AddForce(transform.TransformVector(Vector3.up * force));
+	}
+
+}
