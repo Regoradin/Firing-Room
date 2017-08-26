2200fa6b61038532650fa9a9fe727f1698a019e2
Regoradin <oliver@theabates.com>
2017-08-26 18:44:50 -0500

    Starts Dials Created a base class for dials.

---
+++ Assets/Scripts/Buttons/Dial.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dial : Button {
+
+	public float scroll_increment;
+
+	public float value_increment;
+	public float max_value;
+	public float min_value;
+
+	private float value;
+
+	private void OnMouseOver()
+	{
+		float scroll_amount = Input.GetAxis("Mouse ScrollWheel");
+
+		if (scroll_amount > 0)
+		{
+			ScrollUp(scroll_amount);
+		}
+		if (scroll_amount < 0)
+		{
+			ScrollDown(scroll_amount);
+		}
+	}
+
+	//Rotates around the local Y axis
+	protected void ScrollUp(float scroll_amount)
+	{
+		transform.Rotate(Vector3.right * scroll_increment);
+		value += value_increment;
+		if (value >= max_value)
+		{
+			value = max_value;
+		}
+	}
+
+	protected void ScrollDown(float scroll_amount)
+	{
+		transform.Rotate(Vector3.right * -scroll_increment);
+		value -= value_increment;
+		if (value <= min_value)
+		{
+			value = min_value;
+		}
+	}
+
+	protected virtual void ValueChange()
+	{
+		Debug.Log("Dial " + name + " got changed to " + value);
+	}
+}
