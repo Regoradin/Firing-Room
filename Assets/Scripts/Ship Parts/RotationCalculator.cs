461406682ddb19ed8ddc2c99384df920b6200c6f
Regoradin <oliver@theabates.com>
2017-09-04 15:57:27 -0500

    Rotation Calculator and Controller
    Rotation can  now be controlled, and the ship's transform will properly
    rotate to where it should be relative to velocity. Gimbal lock does
    exist, so for example a full 180 turn must be accomplished with yaw.

---
+++ Assets/Scripts/Ship Parts/RotationCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCalculator : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private Vector3 rotation_from_velocity;
+
+	[HideInInspector]
+	public Vector3 current_rotation;
+
+	private void Awake()
+	{
+		rb = GetComponentInParent<Rigidbody>();
+	}
+
+	private void Start()
+	{
+		rb.velocity = Vector3.up;
+	}
+
+	private void Update()
+	{
+		//Keeps the gyro pointed towards velocity
+		transform.rotation = Quaternion.LookRotation(rb.velocity);
+
+		current_rotation = CalculateRotation();
+		Debug.Log("current_rotation " + current_rotation);
+	}
+
+	private Vector3 CalculateRotation()
+	{
+		Vector3 result = transform.localEulerAngles;
+
+		result.x = result.x > 180 ? result.x - 360 : result.x;
+		result.y = result.y > 180 ? - (result.y - 360) : -result.y;		//this is negated just to make the negative and positive sides more intuitive
+		result.z = result.z > 180 ? result.z - 360 : result.z;
+
+		return result;
+	}
+}
---
+++ Assets/Scripts/Ship Parts/RotationController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+	private RotationCalculator rot_calc;
+
+	public Transform ship_transform;
+
+	public Vector3 rotation_speed;
+	public Vector3 target_rotation;
+
+
+	private void Start()
+	{
+		rot_calc = GetComponentInChildren<RotationCalculator>();
+	}
+
+	private void FixedUpdate()
+	{
+		float x_rot = 0, y_rot = 0, z_rot = 0;
+
+		if(Mathf.Abs(target_rotation.x - rot_calc.current_rotation.x) > rotation_speed.x)
+		{
+			if(target_rotation.x > rot_calc.current_rotation.x)
+			{
+				x_rot = -rotation_speed.x;
+			}
+			else
+			{
+				x_rot = rotation_speed.x;
+			}
+		}
+		if (Mathf.Abs(target_rotation.y - rot_calc.current_rotation.y) > rotation_speed.y)
+		{
+			if (target_rotation.y > rot_calc.current_rotation.y)
+			{
+				y_rot = rotation_speed.y;
+			}
+			else
+			{
+				y_rot = -rotation_speed.y;
+			}
+		}
+		if (Mathf.Abs(target_rotation.z - rot_calc.current_rotation.z) > rotation_speed.z)
+		{
+			if (target_rotation.z > rot_calc.current_rotation.z)
+			{
+				z_rot = -rotation_speed.z;
+			}
+			else
+			{
+				z_rot = rotation_speed.z;
+			}
+		}
+
+		ship_transform.Rotate(new Vector3(x_rot, y_rot, z_rot));
+		Debug.Log("Rotating by " + x_rot + " " + y_rot + " " + z_rot);
+	}
+
+}
