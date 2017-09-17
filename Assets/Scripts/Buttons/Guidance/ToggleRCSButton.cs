8aced2453031a78a4bc20170e844bac35087450e
Regoradin <oliver@theabates.com>
2017-09-17 00:50:50 -0500

    Adds ToggleRCS Button And Task

---
+++ Assets/Scripts/Buttons/Guidance/ToggleRCSButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRCSButton : Button {
+
+	public RCS rcs;
+	private bool state = false;
+
+	public void OnMouseDown()
+	{
+		state = !state;
+		network.AddTask(new ToggleRCSTask(rcs, state));
+	}
+
+}
