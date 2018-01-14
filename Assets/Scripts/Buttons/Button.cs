09e0329da93218c0fd9f8de886fb8a9f0789e65f
Oliver <oliver@theabates.com>
2018-01-14 13:47:43 -0600

    Button Animations + Prefab
    Buttons are now fully animated and a good prefab exists.

--- Assets/Scripts/Buttons/Button.cs
+++ Assets/Scripts/Buttons/Button.cs
@@ -7,6 +7,7 @@ public abstract class Button : NetworkBehaviour {
 
 	protected Network network;
 
+	[Header("Task Settings")]
 	public string category;
 	public float size = 1;
 	public bool send_to_consoles = false;
@@ -16,11 +17,16 @@ public abstract class Button : NetworkBehaviour {
 	private Material mat;
 	new private Light light;
 
+	[Header("Glow Settings")]
 	[ColorUsage(true, true, 0f, 8,.125f, 3f)]
 	public Color unlit_color;
 	[ColorUsage(true, true, 0f, 8, .125f, 3f)]
 	public Color lit_color;
 	private bool lit = false;
+
+	[Header("Animation Settings")]
+	private Animator anim;
+	public bool light_toggle;
 
 	protected void Start()
 	{
@@ -31,22 +37,41 @@ public abstract class Button : NetworkBehaviour {
 		mat.SetColor("_EmissionColor", unlit_color);
 		light = GetComponentInChildren<Light>();
 		light.color = unlit_color;
+
+		anim = GetComponent<Animator>();
 	}
 
 	protected void OnMouseDown()
 	{
-		if (lit)
+		anim.SetTrigger("ButtonPressed");
+	}
+
+	private void ClickEvent()
+	{
+		if (light_toggle)
 		{
-			lit = false;
-			mat.SetColor("_EmissionColor", unlit_color);
-			light.color = unlit_color;
-		}
-		else
-		{
-			lit = true;
-			mat.SetColor("_EmissionColor", lit_color);
-			light.color = lit_color;
+			if (lit)
+			{
+				Unlight();
+			}
+			else
+			{
+				Light();
+			}
 		}
 	}
 
+	public void Light()
+	{
+		lit = true;
+		mat.SetColor("_EmissionColor", lit_color);
+		light.color = lit_color;
+	}
+	public void Unlight()
+	{
+		lit = false;
+		mat.SetColor("_EmissionColor", unlit_color);
+		light.color = unlit_color;
+
+	}
 }
