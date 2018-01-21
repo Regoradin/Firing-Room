c3c88c35e109fa9f2e0674b2c36b712170ec5da5
Regoradin <oliver@theabates.com>
2018-01-21 00:08:52 -0600

    Rudimentary camera resolution scaling
    Resulution of tv cameras can now be changed in the ugliest implementation to ever exist.

--- Assets/FramerateLimiter.cs
+++ Assets/FramerateLimiter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FramerateLimiter : MonoBehaviour {
 
 	private Camera cam;
-	private float fps;
+	private float fps = 1;
 	public float FPS
 	{
 		get { return fps; }
@@ -17,10 +17,19 @@ public class FramerateLimiter : MonoBehaviour {
 		}
 	}
 
+	private RenderTexture texture;
+	public int width = 1;
+	public int height = 1;
+	public Material tv_mat;
+	private int old_width;
+
+
 	private void Start()
 	{
 		cam = GetComponent<Camera>();
 		cam.enabled = false;
+
+		texture = cam.targetTexture;
 
 		InvokeRepeating("Render", 0, 1 / fps);
 	}
@@ -28,5 +37,20 @@ public class FramerateLimiter : MonoBehaviour {
 	private void Render()
 	{
 		cam.Render();
+	}
+
+	private void Update()
+	{
+		if (width != old_width)
+		{
+			RenderTexture rt = new RenderTexture(width, height, 0);
+
+			cam.targetTexture = rt;
+			tv_mat.mainTexture = rt;
+		}
+		old_width = width;
+
+//		texture.width = width;
+//		texture.height = height;
 	}
 }
