8a97350c6daa2db158f3039d34d79cd25cfff30e
Oliver <oliver@theabates.com>
2018-02-08 12:47:00 -0600

    Added MET Clock
    Which will probably be useful for a fair number of things.

---
+++ Assets/Scripts/Ship Parts/METClock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class METClock : NetworkBehaviour, ITriggerTaskable {
+
+	public float time;
+	[SyncVar]
+	private bool running = false;
+
+	public void TriggerTask()
+	{
+		running = !running;
+	}
+
+	private void Update()
+	{
+		if (running)
+		{
+			time += Time.deltaTime;
+		}
+	}
+
+}
